bbcbfd307fde7998902f2d204b4de5dc188c8c4c
frank-liu-toronto <liu1000@gmail.com>
2025-06-06 17:37:15 -0400

    Update Endpoint

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -42,9 +42,24 @@ namespace WebAPIDemo.Controllers
         }
 
         [HttpPut("{id}")]
-        public IActionResult UpdateShirt(int id)
+        [Shirt_ValidateShirtIdFilter]
+        public IActionResult UpdateShirt(int id, Shirt shirt)
         {
-            return Ok($"Updating shirt: {id}");
+            if (id != shirt.ShirtId) return BadRequest(); 
+
+            try
+            {
+                ShirtRepository.UpdateShirt(shirt);
+            }
+            catch
+            {
+                if (!ShirtRepository.ShirtExists(id))
+                    return NotFound();
+
+                throw;
+            }
+
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
