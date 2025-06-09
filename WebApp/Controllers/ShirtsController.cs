2c922ee40ab209e7447224c03bb7a40e6f6e2411
frank-liu-toronto <liu1000@gmail.com>
2025-06-09 13:59:13 -0400

    Retrieve Shirts through WebApi

--- WebApp/Controllers/ShirtsController.cs
+++ WebApp/Controllers/ShirtsController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebApp.Models.Repositories;
+using WebApp.Data;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class ShirtsController : Controller
     {
-        public IActionResult Index()
+        private readonly IWebApiExecuter webApiExecuter;
+
+        public ShirtsController(IWebApiExecuter webApiExecuter)
         {
-            return View(ShirtRepository.GetShirts());
+            this.webApiExecuter = webApiExecuter;
+        }
+
+        public async Task<IActionResult> Index()
+        {            
+            return View(await webApiExecuter.InvokeGet<List<Shirt>>("shirts"));
         }
     }
 }
