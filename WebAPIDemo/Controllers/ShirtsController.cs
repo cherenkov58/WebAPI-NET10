dffb5e4ee357fba9c72e6216393b337b7b16640f
frank-liu-toronto <liu1000@gmail.com>
2025-06-09 10:26:21 -0400

    Implement Delete endpoint with EFCore

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -66,10 +66,12 @@ namespace WebAPIDemo.Controllers
         [TypeFilter(typeof(Shirt_ValidateShirtIdFilterAttribute))]
         public IActionResult DeleteShirt(int id)
         {
-            var shirt = ShirtRepository.GetShirtById(id);
-            ShirtRepository.DeleteShirt(id);
+            var shirtToDelete = HttpContext.Items["shirt"] as Shirt;
+            
+            db.Shirts.Remove(shirtToDelete);
+            db.SaveChanges();
 
-            return Ok(shirt);
+            return Ok(shirtToDelete);
         }
     }
 }
