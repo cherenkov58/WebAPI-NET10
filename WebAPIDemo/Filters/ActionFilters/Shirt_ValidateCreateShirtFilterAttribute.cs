1e919c9f8bbc703eeeca6036df87612b129d3be6
frank-liu-toronto <liu1000@gmail.com>
2025-06-09 10:24:03 -0400

    Implement Put endpoint with EFCore

--- WebAPIDemo/Filters/ActionFilters/Shirt_ValidateCreateShirtFilterAttribute.cs
+++ WebAPIDemo/Filters/ActionFilters/Shirt_ValidateCreateShirtFilterAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using WebAPIDemo.Data;
 using WebAPIDemo.Models;
 using WebAPIDemo.Models.Repositories;
 
@@ -7,6 +8,13 @@ namespace WebAPIDemo.Filters.ActionFilters
 {
     public class Shirt_ValidateCreateShirtFilterAttribute : ActionFilterAttribute
     {
+        private readonly ApplicationDbContext db;
+
+        public Shirt_ValidateCreateShirtFilterAttribute(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             base.OnActionExecuting(context);
@@ -24,7 +32,20 @@ namespace WebAPIDemo.Filters.ActionFilters
             }
             else
             {
-                var existingShirt = ShirtRepository.GetShirtByProperties(shirt.Brand, shirt.Gender, shirt.Color, shirt.Size);
+                var existingShirt = db.Shirts.FirstOrDefault(x =>
+                    !string.IsNullOrWhiteSpace(shirt.Brand) &&
+                    !string.IsNullOrWhiteSpace(x.Brand) &&
+                    x.Brand.ToLower() == shirt.Brand.ToLower() &&
+                    !string.IsNullOrWhiteSpace(shirt.Gender) &&
+                    !string.IsNullOrWhiteSpace(x.Gender) &&
+                    x.Gender.ToLower() == shirt.Gender.ToLower() &&
+                    !string.IsNullOrWhiteSpace(shirt.Color) &&
+                    !string.IsNullOrWhiteSpace(x.Color) &&
+                    x.Color.ToLower() == shirt.Color.ToLower() &&
+                    shirt.Size.HasValue &&
+                    x.Size.HasValue &&
+                    shirt.Size.Value == x.Size.Value);
+
                 if (existingShirt != null)
                 {
                     context.ModelState.AddModelError("Shirt", "Shirt already exists.");
