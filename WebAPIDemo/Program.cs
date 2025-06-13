9d484378eaacc305bd5e0b49ecd28473550a58d4
frank-liu-toronto <liu1000@gmail.com>
2025-06-13 13:02:39 -0400

    OpenAPI Support & Swagger UI

--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -11,9 +11,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "WebAPIDemo API v1");
+    });
+}
 
 app.UseHttpsRedirection();
 
