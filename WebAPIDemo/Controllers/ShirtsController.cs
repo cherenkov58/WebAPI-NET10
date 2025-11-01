c5b5680f9d4dd2092b07a7dab53f9a4729f521b4
Marina Cherenkova <Marina.Cherenkova@crawco.ca>
2025-11-01 12:52:13 -0400

    some comments

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -16,7 +16,7 @@ namespace WebAPIDemo.Controllers
             new Shirt { ShirtId = 1, Brand = "My Brand", Color = "Blue", Gender = "Men", Price = 30, Size = 10 },
             new Shirt { ShirtId = 2, Brand = "My Brand", Color = "Black", Gender = "Men", Price = 35, Size = 12 },
             new Shirt { ShirtId = 3, Brand = "Your Brand", Color = "Pink", Gender = "Women", Price = 28, Size = 8 },
-            new Shirt { ShirtId = 4, Brand = "Your Brand", Color = "Yello", Gender = "Women", Price = 30, Size = 9 }
+            new Shirt { ShirtId = 4, Brand = "Your Brand", Color = "Yellow", Gender = "Women", Price = 30, Size = 9 }
         };
 
         [HttpGet]
@@ -25,11 +25,25 @@ namespace WebAPIDemo.Controllers
             return Ok(ShirtRepository.GetShirts());
         }
 
-        [HttpGet("{id}")]
+		/*	[HttpGet("{id}")]
+	    public string GetShirtsBiId(int id,[FromHeader(Name = "Color")] string color)
+	    {
+		    return $"Reading shirt: {id}, color: {color}";
+	    }
+	    */
+
+		[HttpGet("{id}")]
         [Shirt_ValidateShirtIdFilter]
         public IActionResult GetShirtById(int id)
         {
-            return Ok(ShirtRepository.GetShirtById(id));
+			/*if (id <= 0) 
+			return BadRequest();
+
+		    var shirt= ShirtRepository.GetShirtById(id);
+		    if (shirt == null)
+			    return NotFound();*/
+
+			return Ok(ShirtRepository.GetShirtById(id));
         }
 
         [HttpPost]
