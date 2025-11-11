671ab42395db592f93d0d09cfc0afefce8129bc1
Marina Cherenkova <Marina.Cherenkova@crawco.ca>
2025-11-11 11:02:10 -0500

    final changes section 5

--- WebApp/Controllers/ShirtsController.cs
+++ WebApp/Controllers/ShirtsController.cs
@@ -87,9 +87,17 @@ namespace WebApp.Controllers
 
         [HttpPost]
         public async Task<IActionResult> DeleteShirt([FromForm] int shirtId)
-        {
-            await webApiExecuter.InvokeDelete($"shirts/{shirtId}");
-            return RedirectToAction(nameof(Index));
+		{
+            try
+            {
+                await webApiExecuter.InvokeDelete($"shirts/{shirtId}");
+                return RedirectToAction(nameof(Index));
+            }
+            catch (WebApiException ex)
+            {
+                HandleWebApiException(ex);
+                return View(nameof(Index), await webApiExecuter.InvokeGet<List<Shirt>>("shirts"));
+			}
 		}
 
         private void HandleWebApiException(WebApiException ex)
