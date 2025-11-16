cb8436fa3f2f83777acc2945c1aabf6197956fbe
Marina Cherenkova <Marina.Cherenkova@crawco.ca>
2025-11-16 07:00:29 -0500

    database update

---
+++ WebAPIDemo/Migrations/20251116113540_AddDescriptionToShirt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToShirt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
